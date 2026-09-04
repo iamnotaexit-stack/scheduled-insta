ff874e02734d8e94a57fcc1959285cc2139216e9
AdityaShukla-iitg <aditya0191@op.iitg.ac.in>
2026-09-04 15:19:29 +0530

    Add new Instagram reel link to config.py

--- config.py
+++ config.py
@@ -3,7 +3,13 @@
 
 REEL_LINKS = [
     "https://www.instagram.com/reel/Dc0U9eCRyq4/",
-    "https://www.instagram.com/reel/Dc0Qg8WxenI/"
+    "https://www.instagram.com/reel/Dc0Qg8WxenI/",
+    "https://www.instagram.com/reel/Dc3EfT-Rl_h/"
 ]
 
 PROFILE_LINK = "https://www.instagram.com/fluxiolive/"
+
+# Schedule intervals in seconds
+VIEWS_INTERVAL = 7 * 60                  # 7 minutes
+LIKES_INTERVAL = 35 * 60                 # 35 minutes
+FOLLOWERS_INTERVAL = 24 * 3600 + 3 * 60  # 24 hours 3 minutes
